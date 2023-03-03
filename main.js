c37de25064803166530639572fee2656f9b1a541
Sergey Koksharov <gentoosiastic@gmail.com>
2023-03-03 15:56:25 +0300

    feat: replace image comparison slider with better version

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
+import Reveal from "reveal.js";
+import RevealNotes from "reveal.js/plugin/notes/notes";
 import "reveal.js/dist/reveal.css";
 // see available themes in the
 // node_modules/reveal.js/dist/theme
 //  beige, black, blood, league, moon, night, serif, simple, ...
+// import ComparisonSlider from "comparison-slider";
 import "reveal.js/dist/theme/moon.css";
-import Reveal from "reveal.js";
-import ComparisonSlider from "comparison-slider";
-import RevealNotes from "reveal.js/plugin/notes/notes";
 
 const deck = new Reveal();
-deck.initialize({ hash: true, slideNumber: true
-  , plugins: [ RevealNotes ] 
-});
-const comparisonSlider = new ComparisonSlider();
+deck.initialize({ hash: true, slideNumber: true, plugins: [RevealNotes] });
